ecacbc97436fe9d1934f2a9419bb2da5ef6c5b48
Michel Oliveira <michelsantosoliveira@hotmail.com>
2021-11-20 14:52:24 -0300

    Requisito 5 ainda em andamento

--- src/context/SearchPlanetsProvider.js
+++ src/context/SearchPlanetsProvider.js
@@ -5,9 +5,13 @@ import SearchPlanetsContext from './SearchPlanetsContext';
 
 const SearchPlanetsProvider = ({ children }) => {
   const [planets, setPlanets] = useState([]);
-  const [column, setColumn] = useState('');
-  const [comparison, setComparison] = useState('maior que');
-  const [value, setValue] = useState('');
+  const [select, setSelect] = useState({
+    population: '',
+    orbital_period: '',
+    diameter: '',
+    rotation_period: '',
+    surface_water: '',
+  });
   const [filter, setFilter] = useState({
     filters: {
       filterByName: {
@@ -16,6 +20,9 @@ const SearchPlanetsProvider = ({ children }) => {
       filterByNumericValues: [],
     },
   });
+
+  const { filters: { filterByNumericValues } } = filter;
+  console.log(filterByNumericValues);
 
   const fetchData = async () => {
     const { results } = await getPlanetSearch();
@@ -26,17 +33,63 @@ const SearchPlanetsProvider = ({ children }) => {
     fetchData();
   }, []);
 
+  const handleFilter = () => {
+    console.log(filterByNumericValues);
+    let filteredPlanets = planets;
+    if (filterByNumericValues[filterByNumericValues.length - 1].comparison === 'menor que') {
+      filteredPlanets = planets
+        .filter((dado) => Number(dado[filterByNumericValues[filterByNumericValues.length - 1].column]) < Number((filterByNumericValues[filterByNumericValues.length - 1].value)));
+    }
+    if (filterByNumericValues[filterByNumericValues.length - 1].comparison === 'maior que') {
+      filteredPlanets = planets
+        .filter((dado) => Number(dado[filterByNumericValues[filterByNumericValues.length - 1].column]) > Number(filterByNumericValues[filterByNumericValues.length - 1].value));
+    }
+    if (filterByNumericValues[filterByNumericValues.length - 1].comparison === 'igual a') {
+      filteredPlanets = planets
+        .filter((dado) => Number(dado[filterByNumericValues[filterByNumericValues.length - 1].column]) === Number(filterByNumericValues[filterByNumericValues.length - 1].value));
+    }
+    setPlanets(filteredPlanets);
+
+    if (filterByNumericValues[filterByNumericValues.length - 1].column === 'population') {
+      setSelect({
+        ...select,
+        population: 'clickPopulation',
+      });
+    }
+
+    if (filterByNumericValues[filterByNumericValues.length - 1].column === 'orbital_period') {
+      setSelect({
+        ...select,
+        orbital_period: 'clickOrbital',
+      });
+    }
+    if (filterByNumericValues[filterByNumericValues.length - 1].column === 'diameter') {
+      setSelect({
+        ...select,
+        diameter: 'clickDiameter',
+      });
+    }
+    if (filterByNumericValues[filterByNumericValues.length - 1].column === 'rotation_period') {
+      setSelect({
+        ...select,
+        rotation_period: 'clickRotation',
+      });
+    }
+    if (filterByNumericValues[filterByNumericValues.length - 1].column === 'surface_water') {
+      setSelect({
+        ...select,
+        surface_water: 'clickSurface',
+      });
+    }
+  };
+
   const context = {
     planets,
     setPlanets,
     filter,
     setFilter,
-    column,
-    setColumn,
-    comparison,
-    setComparison,
-    value,
-    setValue,
+    select,
+    handleFilter,
   };
 
   return (
